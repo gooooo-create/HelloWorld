41a6feba87b4f6ce0e036e69579b46a84fee6d6f
gooooo-create <210103956@qq.com>
2026-07-08 11:48:50 +0800

    fourth commit

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,7 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello 01xingqiu!" << std::endl;
+    std::cout << "nihao 01xingqiu!" << std::endl;
 
     return 0;
 }
